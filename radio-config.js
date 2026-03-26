a2d66fc8721b2bc88522440d868dc2b121889ae8
Jeremy-MegaJS <elalienjeremy@gmail.com>
2026-03-26 12:33:49 -0600

    Update radio tagline and logo path

--- radio-config.js
+++ radio-config.js
@@ -1,14 +1,14 @@
 window.RADIO_CONFIG = {
     name: "Radio Fe 990 AM",
-    tagline: "'Una radio que une a creyentes con la Palabra de Dios'",
+    tagline: "Una radio que une creyentes con la palabra",
     description: "Es una emisora dedicada a edificar vidas a través de la Palabra de Dios. En nuestra programación encontrarás alabanzas que inspiran el alma, predicaciones que fortalecen la Fe y música que transforma corazones.",
     schedule: "Lunes a domingo, de 6:00 a.m. a 8:00 p.m.",
     streamUrl: "http://104.238.162.149:8000/radioFe",
-    logo: "logo.jpeg",
+    logo: "img-sources/logo.jpeg",
     colors: {
         bg: "#182241",
-        surface: "#000000 ",
-        primary: "#cfaf78 ",
-        text: "#d3c8c1"
+        surface: "#000000",
+        primary: "#d3c8c1",
+        text: "#dbbaa5"
     }
 };
