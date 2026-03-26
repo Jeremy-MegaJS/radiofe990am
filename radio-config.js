958e75ab968656d3f7ed23092dc22e234d16b451
Jeremy-MegaJS <elalienjeremy@gmail.com>
2026-03-25 19:01:53 -0600

    Update logo path in radio configuration

--- radio-config.js
+++ radio-config.js
@@ -4,7 +4,7 @@ window.RADIO_CONFIG = {
     description: "Es una emisora dedicada a edificar vidas a través de la Palabra de Dios. En nuestra programación encontrarás alabanzas que inspiran el alma, predicaciones que fortalecen la Fe y música que transforma corazones.",
     schedule: "Lunes a domingo, de 6:00 a.m. a 8:00 p.m.",
     streamUrl: "http://104.238.162.149:8000/radioFe",
-    logo: "img-sources/logo.jpeg",
+    logo: "logo.jpeg",
     colors: {
         bg: "#182241",
         surface: "#000000 ",
